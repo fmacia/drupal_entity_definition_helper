8d0c94ecacdd4c21421631b05e64ee5317b42757
fmacia <fjmaciaespin@gmail.com>
2022-03-22 20:10:41 +0100

    feat: Added button component

---
+++ src/components/vButton.vue
@@ -0,0 +1,8 @@
+<script setup>
+</script>
+
+<template>
+  <a href="#" class="block w-fit rounded py-4 px-6 shadow bg-sky-400 dark:bg-yellow-400 text-white dark:text-black">
+    <slot></slot>
+  </a>
+</template>
