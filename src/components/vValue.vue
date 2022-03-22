59a28f8c9b15b1ffab91f5b055a5e2923c964a50
fmacia <fjmaciaespin@gmail.com>
2022-03-22 23:29:07 +0100

    feat: Added Value component

---
+++ src/components/vValue.vue
@@ -0,0 +1,21 @@
+<script setup>
+// Props
+const props = defineProps({
+  label: String,
+  value: String,
+  hideEmpty: {
+    type: Boolean,
+    default: true,
+  },
+})
+</script>
+
+<template>
+  <div v-if="!hideEmpty || value">
+    <span class="font-bold">
+      {{ label }}:
+    </span>
+
+    {{ value }}
+  </div>
+</template>
