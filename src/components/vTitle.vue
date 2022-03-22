0d8bdf3a19f3c7f744481cd47334a9276e0b3b12
fmacia <fjmaciaespin@gmail.com>
2022-03-22 23:30:04 +0100

    feat: Title component, border property

--- src/components/vTitle.vue
+++ src/components/vTitle.vue
@@ -2,14 +2,19 @@
 import { computed } from 'vue'
 
 const props = defineProps({
-  type: String
+  type: String,
+  border: {
+    type: Boolean,
+    default: true,
+  },
 })
 
 const classObject = computed(() => ({
   'font-bold text-3xl mb-8': props.type == 'h1',
-  'inline text-2xl font-bold border-b-2 dark:border-b-yellow-300 border-sky-400': props.type == 'h2',
-  'text-xl font-bold mb-4': props.type == 'h3',
+  'text-2xl font-bold': props.type == 'h2',
+  'text-xl font-bold mb-8': props.type == 'h3',
   'text-lg font-bold': props.type == 'h4',
+  'w-fit border-b-2 dark:border-b-yellow-300 border-sky-400': props.border == true,
 }))
 </script>
 
